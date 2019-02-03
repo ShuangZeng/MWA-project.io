056f23147a6b9c1195fe1cb25e4349cbb72efae1
Festus Iipito <dev3320770@gmail.com>
2019-02-03 05:44:55 -0600

    backend express restful app

--- backend-express/app.js
+++ backend-express/app.js
@@ -3,24 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var questions = require('./routes/questions');
+var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('strict routing', true);
+app.set('port', process.env.PORT || 3000);
+const port = app.get('port');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/questions', questions);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +43,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
+app.listen(3000, ()=> console.log(`listing to port ${port}`))
