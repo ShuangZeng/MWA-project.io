40f281b89b453506fc6b0f3924e1bd27c9cec17b
Laith Mihyar <LaithMihyar@Laiths-MacBook-Pro.local>
2019-02-03 15:15:16 -0600

    change res object

--- backend-express/routes/auth.js
+++ backend-express/routes/auth.js
@@ -4,25 +4,20 @@ const {User} = require("../model/user");
 const express = require("express");
 const router = express.Router();
 
-
-
 router.post("/", async (req, res) =>  {
    var email = req.body.email;
     let user =  await User.findOne({email:email});
-
-
-    console.log(user.password);
     if(!user){
-        return res.status(400).send("Invalid Email or Password");
+        return res.json({status: 400, message:"Invalid email"});
     };
 
     const validPassword = bcrypt.compare(req.body.password, user.password);4
      if(!validPassword){
-        return res.status(400).send("Invalid Email or Password");
+        return res.json({status: 400, message:"Invalid password"});
      }
      const token = user.generateAuthToken();
      
-     res.status(200).json(token);
+     return res.json({status: 200, message: token});
 
 }); 
 
