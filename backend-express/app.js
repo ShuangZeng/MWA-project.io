5076332e6fda182e5c3ef38e9de51d64b9599f2f
Festus Iipito <dev3320770@gmail.com>
2019-02-03 22:25:12 -0600

    connections string update (dot env)

--- backend-express/app.js
+++ backend-express/app.js
@@ -30,7 +30,7 @@ app.use(helmet())
 //    console.error('FATAL ERROR: jwtPrivateKey is not defined')
 //    process.exit(1);
 // }
-// const connect = mongoose.connect("mongodb+srv://sysadmin:Admin2020@tutaappcluster-ho95w.mongodb.net/team4?retryWrites=true" ,{useNewUrlParser:true})
+// const connect = mongoose.connect("mongodb+srv://<username>:<pass>@tutaappcluster-ho95w.mongodb.net/team4?retryWrites=true" ,{useNewUrlParser:true})
 // .then(()=> console.log("Connected to DB!"))
 // .catch(error => console.log("Couldnt connect to DB!", error.message));
 const mongooseConnect = require("./config/config");
--- backend-express/config/config.js
+++ backend-express/config/config.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
 
-const connect = mongoose.connect("mongodb+srv://sysadmin:Admin2020@tutaappcluster-ho95w.mongodb.net/team4?retryWrites=true" ,{useNewUrlParser:true})
+const connect = mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/team4?retryWrites=true` ,{useNewUrlParser:true})
 .then(()=> console.log("Connected to DB!"))
 .catch(error => console.log("Couldnt connect to DB!", error.message));
 
