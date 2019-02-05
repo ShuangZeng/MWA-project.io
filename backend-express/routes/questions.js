54b42c0bcb1b67ab8ef4f8fdce22e66dae29a98c
Laith Mihyar <LaithMihyar@Laiths-MacBook-Pro.local>
2019-02-04 21:45:22 -0600

    some changes

--- backend-express/routes/questions.js
+++ backend-express/routes/questions.js
@@ -3,47 +3,63 @@ var router = express.Router();
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const mongoose = require("mongoose");
-const questionSchema = new mongoose.Schema({}, { strict: false });
+const questionSchema = new mongoose.Schema({}, {
+  strict: false
+});
 const questions = mongoose.model('questions', questionSchema);
 
 
-router.get('/',auth, admin, async(req, res, next)=> {
+router.get('/', auth, admin, async (req, res, next) => {
   const results = await questions.find();
   res.json(results);
 });
 
-router.get('/:id',auth, admin, async (req, res, next)=> {
-  const results = await questions.findById({ _id: req.params.id});
- 
+router.get('/:id', auth, admin, async (req, res, next) => {
+  const results = await questions.findById({
+    _id: req.params.id
+  });
+
   res.json(results);
 });
 
-router.get('/tags/:tags',auth, admin, async (req, res, next)=> {
+router.get('/tags/:tags', auth, admin, async (req, res, next) => {
   const results = await questions.find({}).where('tags').in([req.params.tags]);
- 
+
   res.json(results);
 });
 
 
-router.post('/',auth,admin, async (req, res, next)=> {
+router.post('/', auth, admin, async (req, res, next) => {
   const tmp = req.body
   tmp.dateCreated = new Date();
   var record = new questions(tmp)
   record.save();
- 
+
   res.json('success');
 });
 
-router.delete('/',auth,admin, async (req, res, next)=> {
+router.delete('/', auth, admin, async (req, res, next) => {
   const results = await questions.findByIdAndRemove(req.body);
- 
+
   res.json(results);
 });
 
-router.patch('/:id', auth,admin,async (req, res, next)=> {
-  const results = await questions.update({'_id': req.params.id},{$set : req.body});
- 
+router.patch('/:id', auth, admin, async (req, res, next) => {
+  const results = await questions.update({
+    '_id': req.params.id
+  }, {
+    $set: req.body
+  });
+
   res.json(results);
 });
+
+router.get('/test/random', async (req, res) => {
+
+  const results = await questions.find({}).limit(3);
+
+  res.json(results)
+
+})
 
 module.exports = router;
